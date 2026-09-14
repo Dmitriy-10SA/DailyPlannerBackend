db5dbbd18c53a2a69ec3aed41a7c5a5452e34a07
dsemkin <semkin_dmitriy10@vk.com>
2026-09-14 13:43:26 +0400

    Init frontend folder

---
+++ frontend/shared/src/commonMain/kotlin/com/andef/daily/planner/frontend/App.kt
@@ -0,0 +1,26 @@
+package com.andef.daily.planner.frontend
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+/**
+ * Корневой компонент приложения
+ */
+@Composable
+@Preview
+fun App() {
+    MaterialTheme {
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Text("Daily Planner")
+        }
+    }
+}
